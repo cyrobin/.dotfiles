65d1de28c4491373fb1cac6512dca5c7b6770732
Cyril Robin <cyril.robin@laas.fr>
2013-08-26 18:04:49 +0200

    Use to handle a new config easily
    This current script has NOT been tested yet !

---
+++ bootstrap.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+
+###### Become sudoer
+#su ; visudo ; # add "mylogin ALL=(ALL)ALL" at the end
+echo "!!! Warning : you'll need to be sudoer."
+echo "Tips : 'su ; visudo ; ' then add '<yourlogin> ALL=(ALL)ALL' at the end"
+
+###### Memo about the source.list
+echo "!!! Warning : Please check your source.list"
+echo "'sudo vi /etc/apt/source.list' : check for contrib and non-free"
+
+echo "*** Bootstrapping..."
+
+###### Update/Upgrade of the system
+echo "*** Updating the system..."
+sudo apt-get update
+
+echo "*** Upgrading the system..."
+sudo apt-get upgrade
+
+###### Get the main desired packages
+echo "*** Installing packages..."
+sudo apt-get install \
+zsh vim vim-gtk git tig colordiff\
+texlive-full rubber tmux\
+make make-doc cmake-curses-gui\
+awesome awesome-extra slim conky\
+i3lock libnotify-bin xfonts-terminus ttf-dejavu xcompmgr alsa-utils \
+pavucontrol gnome-bluetooth pulseaudio numlockx rxvt-unicode-256color \
+xbacklight wicd cmus \
+python-fontforge filezilla calibre \
+system-config-printer \
+mutt-patched urlview \
+offlineimap msmtp \
+ldbd libnet-ldap-perl \
+ffmpeg gparted unetbootin
+
+###### Other proprietary packages
+## For Intel wifi : 
+# apt-get install firmware-iwlwifi ;
+# modprobe -r iwlwifi ; modprobe iwlwifi
+## For Mendeley desktop : check the website
+## For Dropbox
+
+###### Set up the shell
+echo "*** Setting zsh as default login shell..."
+chsh -s /bin/zsh $(whoami)
+
+###### Copy config dot files
+DOTFILES=".zshrc .gitconfig"
+
+echo "*** Backuping dotfiles: $DOTFILES ..."
+mkdir -p ~/backup_dotfiles
+DATE_BAK=$(date +%s)
+for dotfile in $DOTFILES; do
+    [[ -f ~/${dotfile} ]] && mv ~/${dotfile} ~/backup/bak.${DATE_BAK}${dotfile}
+done
+
+echo "*** Copying dotfiles to HOME..."
+cd ~/.dotfiles # git clone git://github.com/cyrobin/dotfiles.git ~/.dotfiles
+cp -r $DOTFILES ~
+
+###### Handle vim config
+echo "*** Handling vim config..."
+git clone git://github.com/cyrobin/.vim.git ~/.vim
+sh ~/.vim/.install.sh
+
+###### Handle awesome config
+echo "*** Handling awesome config..."
+git clone git://github.com/cyrobin/awesome-wm ~/.config/awesome
+
+###### Handle muttrc config
+#echo "*** Handling awesome config..."
+#TODO
+
+###### Handle Todo.txt ?
+#+Dropbox or HubiC for sync ?
+
+###### Get useful scripts
+#TODO
+#echo "Getting useful scripts..."
+#git clone git://github.com/cyrobin/awesome-wm ~/.config/awesome
+# also : todo.txt ?
+
+
+###### Remove the @$!# beeps from the speaker
+echo "*** Disabling the aggressive beeps"
+sudo rmmod pcspkr
+sudo echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+
+###### Cron tasks
+#TODO
+# Use crontab -e and : 
+# Mail quick check every couple of minutes
+# 4-59/2 * * * * /usr/bin/offlineimap -q > /dev/null 2>&1
+# Mail full check every hour
+# 0 * * * * /usr/bin/offlineimap > /dev/null 2>&1
+# Rsync LAAS Backup
+# 0 13 * * * sh /home/crobin/dev/scripts/make_backup_LAAS.sh
+
+#
+
+
+
+###### Creating other desired repositories
+mkdir $HOME/dev
+
+echo "*** Done. Welcome Home ! =)"
+
+#
+#echo "Make home folders"
+#mkdir work devel openrobots sandbox tmp
+#
+#[[ "$?" != "0" ]] && echo "Looks like you're already setup" && exit 1
+#
